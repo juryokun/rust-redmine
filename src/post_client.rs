86c6b683f741ad9df113da7f48aca8428e7e23bd
juryokun <capeta.kozo@gmail.com>
2021-09-12 23:28:38 +0900

    modify post

--- src/post_client.rs
+++ src/post_client.rs
@@ -1,39 +1,79 @@
 use serde::{Deserialize, Serialize};
 
-async fn post_request() -> Result<(), Box<dyn std::error::Error>> {
-    let issue = NewIssue::new();
-    // let json = serde_json::to_string_pretty(&issue).unwrap();
-    // println!("{}", json);
-
-    // let client = reqwest::Client::new();
-    let client = reqwest::Client::builder().build()?;
-    // let url = "http://localhost:8080/projects/company/issues.json?query_id=1";
-    let url = "http://localhost:8080/projects/company/issues.json";
-
-    let response = client
-        .post(url)
-        // .body(json)
-        .json(&issue)
-        .header(
-            "X-Redmine-API-Key",
-            "d44bf19219dbd92d882ef5a7063b71f01e8fc6b3",
-        )
-        // .header(reqwest::header::CONTENT_TYPE, "application/json")
-        .send()
-        .await?;
-    let result = response.text().await;
-    println!("{:?}", result);
-    // println!("{:?}", response);
-
-    Ok(())
+#[derive(Debug, Default)]
+pub struct PostClient {
+    url: String,
+    api_key: String,
+    user: String,
+    password: String,
+    data: NewIssue,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+impl PostClient {
+    pub fn builder() -> PostClientBuilder {
+        PostClientBuilder::default()
+    }
+    pub async fn send(self) {
+        let client = reqwest::Client::new();
+
+        let response = client
+            .post(self.url)
+            .json(&self.data)
+            .header("X-Redmine-API-Key", self.api_key)
+            .send()
+            .await
+            .unwrap();
+        let result = response.text().await;
+        println!("{:?}", result);
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct PostClientBuilder {
+    url: String,
+    api_key: String,
+    user: String,
+    password: String,
+    data: NewIssue,
+}
+
+impl PostClientBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    pub fn url(mut self, url: String) -> Self {
+        self.url = url;
+        self
+    }
+    pub fn key(mut self, key: String) -> Self {
+        self.api_key = key;
+        self
+    }
+    pub fn user(mut self, user: String) -> Self {
+        self.user = user;
+        self
+    }
+    pub fn password(mut self, password: String) -> Self {
+        self.password = password;
+        self
+    }
+    pub fn build(self) -> PostClient {
+        PostClient {
+            url: self.url,
+            api_key: self.api_key,
+            user: self.user,
+            password: self.password,
+            data: self.data,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, Default)]
 struct NewIssue {
     issue: NewIssueContent,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Default)]
 struct NewIssueContent {
     project_id: i64,
     tracker_id: i64,
